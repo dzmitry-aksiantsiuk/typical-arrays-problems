b527cde6dc321193cc37ef1b9afab03541802b8e
Dzmitry Aksiantsiuk <dzmitry.aksiantsiuk@gmail.com>
2020-09-30 22:10:00 +0300

    feat: implement three functions search number

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  const minValue = Math.min.apply(null, array)
+  return minValue !== Infinity ? minValue : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  const maxValue = Math.max.apply(null, array)
+  return maxValue !== -Infinity ? maxValue : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == null || array.length === 0) return 0;
+  return array.reduce((acc, item) => acc + item) / array.length;
 }
